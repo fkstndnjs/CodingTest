5468708d64f09d2df0ed402df2c328bc0fbaf951
fkstndnjs <fkstndnjs@naver.com>
2023-10-09 22:15:56 +0900

    K번째 큰 수

--- lv2/ex17.py
+++ lv2/ex17.py
@@ -1,11 +1,16 @@
 import sys
 input_file_path = "/Users/ysh/Documents/Git Repository/CodingTest/lv2/input.txt"
 sys.stdin=open(input_file_path, "rt")
-T=int(input())
 
-for t in range(T):
-    n,s,e,k=map(int, input().split())
-    a=list(map(int, input().split()))
-    newA = a[s-1:e]
-    newA.sort()
-    print("#%d %d" %(t+1, newA[k-1]))
+n, k = map(int, input().split())
+a=list(map(int, input().split()))
+
+res=set()
+for i in range(n):
+    for j in range(i+1, n):
+        for m in range(j+1, n):
+            res.add(a[i]+a[j]+a[m])
+
+res=list(res)
+res.sort(reverse=True)
+print(res[k-1])
