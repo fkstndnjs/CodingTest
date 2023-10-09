8cb6806ec286ae9d310fd1fdc8041f0301cc9442
fkstndnjs <fkstndnjs@naver.com>
2023-10-10 04:51:45 +0900

    점수 계산

--- lv2/ex17.py
+++ lv2/ex17.py
@@ -3,13 +3,16 @@ input_file_path = "/Users/ysh/Documents/Git Repository/CodingTest/lv2/input.txt"
 sys.stdin=open(input_file_path, "rt")
 
 n = int(input())
-ch=[0]*(n+1)
+a=list(map(int, input().split()))
+
+sum=0
 cnt=0
 
-for i in range(2, n+1):
-    if ch[i]==0:
+for x in a:
+    if x==1:
         cnt+=1
-        for j in range(i, n+1, i):
-            ch[j]=1
+        sum+=cnt
+    else:
+        cnt=0
 
-print(cnt)
+print(sum)
