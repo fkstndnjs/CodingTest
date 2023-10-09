27831dc50222a47c7aa0a6aa3db370709a1db33a
fkstndnjs <fkstndnjs@naver.com>
2023-10-10 07:58:58 +0900

    사과나무

--- lv2/ex17.py
+++ lv2/ex17.py
@@ -5,18 +5,18 @@ sys.stdin=open(input_file_path, "rt")
 n=int(input())
 li = [list(map(int, input().split())) for _ in range(n)]
 
-largest=0
+res=0
+s=e=n//2
 
 for i in range(n):
-    sum1=sum2=sum3=sum4=0
+    for j in range(s, e+1):
+        res+=li[i][j]
 
-    for j in range(n):
-        sum1+=li[i][j]
-        sum2+=li[j][i]
-        
-    sum3+=li[i][i]
-    sum4+=li[i][n-i-1]
+    if i<n//2:
+        s-=1
+        e+=1
+    else:
+        s+=1
+        e-=1
 
-    largest = max(largest, sum1, sum2, sum3, sum4)
-
-print(largest)
+print(res)
