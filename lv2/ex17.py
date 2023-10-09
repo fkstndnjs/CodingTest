f26338c247cf9f88651d35e0bdf990d50c2b67c8
fkstndnjs <fkstndnjs@naver.com>
2023-10-10 04:32:01 +0900

    소수 개수 구하기

--- lv2/ex17.py
+++ lv2/ex17.py
@@ -3,23 +3,13 @@ input_file_path = "/Users/ysh/Documents/Git Repository/CodingTest/lv2/input.txt"
 sys.stdin=open(input_file_path, "rt")
 
 n = int(input())
-a = list(map(int, input().split()))
+ch=[0]*(n+1)
+cnt=0
 
-def digit_sum(x):
-    sum=0
+for i in range(2, n+1):
+    if ch[i]==0:
+        cnt+=1
+        for j in range(i, n+1, i):
+            ch[j]=1
 
-    while x>0:
-        sum+=x%10
-        x=x//10
-    
-    return sum
-
-max=-2147000000
-
-for x in a:
-    tot=digit_sum(x)
-    if tot>max:
-        max=tot
-        res=x
-
-print(res)
+print(cnt)
