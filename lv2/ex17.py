bea6cecaaee6690757f86345c132b3e4c3f01c94
fkstndnjs <fkstndnjs@naver.com>
2023-10-09 21:59:04 +0900

    K번째 수

--- lv2/ex17.py
+++ lv2/ex17.py
@@ -1,16 +1,11 @@
 import sys
 input_file_path = "/Users/ysh/Documents/Git Repository/CodingTest/lv2/input.txt"
 sys.stdin=open(input_file_path, "rt")
-n, k=map(int, input().split())
-print(n,k)
+T=int(input())
 
-cnt=0
-for i in range(1, n+1):
-    if n%i==0:
-        cnt+=1
-    if cnt==k:
-        print(i)
-        break
-else:
-    print(-1)
-
+for t in range(T):
+    n,s,e,k=map(int, input().split())
+    a=list(map(int, input().split()))
+    newA = a[s-1:e]
+    newA.sort()
+    print("#%d %d" %(t+1, newA[k-1]))
