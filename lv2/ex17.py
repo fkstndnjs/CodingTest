f6db8153a44b3a9e2f9a8956ceb0e2f3e1f3f2e1
fkstndnjs <fkstndnjs@naver.com>
2023-10-10 17:06:57 +0900

    격자판 회문수

--- lv2/ex17.py
+++ lv2/ex17.py
@@ -1,21 +1,18 @@
 import sys
 input_file_path = "/Users/ysh/Documents/Git Repository/CodingTest/lv2/input.txt"
 sys.stdin=open(input_file_path, "rt")
-dx=[-1, 0, 1, 0]
-dy=[0,1,0,-1]
 n=int(input())
 li = [list(map(int, input().split())) for _ in range(n)]
-li.insert(0, [0]*n)
-li.append([0]*n)
-for l in li:
-    l.insert(0, 0)
-    l.append(0)
+# 입력값
+# -----------------------------------------------------------------
 
 cnt=0
 
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        if all(li[i][j] > li[i+dx[k]][j+dy[k]] for k in range(4)):
+for i in range(3):
+    for j in range(7):
+        tmp=li[j][i:i+5]
+        if tmp==tmp[::-1]:
+            print(tmp)
             cnt+=1
 
 print(cnt)
