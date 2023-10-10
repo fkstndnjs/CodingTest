c1ea21cb6365c095de9cc4ca6ea0dd7fb8f0f8ae
fkstndnjs <fkstndnjs@naver.com>
2023-10-10 17:17:06 +0900

    이분 탐색

--- lv2/ex17.py
+++ lv2/ex17.py
@@ -1,18 +1,20 @@
 import sys
 input_file_path = "/Users/ysh/Documents/Git Repository/CodingTest/lv2/input.txt"
 sys.stdin=open(input_file_path, "rt")
-n=int(input())
-li = [list(map(int, input().split())) for _ in range(n)]
+n, m = map(int, input().split())
+a=list(map(int, input().split()))
 # 입력값
 # -----------------------------------------------------------------
 
-cnt=0
-
-for i in range(3):
-    for j in range(7):
-        tmp=li[j][i:i+5]
-        if tmp==tmp[::-1]:
-            print(tmp)
-            cnt+=1
-
-print(cnt)
+a.sort()
+lt=0
+rt=n-1
+while lt<=rt:
+    mid = (lt+rt)//2
+    if a[mid]==m:
+        print(mid+1)
+        break
+    elif a[mid]>m:
+        rt=mid-1
+    else:
+        lt=mid+1
