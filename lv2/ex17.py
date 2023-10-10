2173e65c246b305e4fc868f27c2ef922aa532db6
fkstndnjs <fkstndnjs@naver.com>
2023-10-10 21:43:59 +0900

    바이러스

--- lv2/ex17.py
+++ lv2/ex17.py
@@ -1,21 +1,28 @@
 import sys
 input_file_path = "/Users/ysh/Documents/Git Repository/CodingTest/lv2/input.txt"
 sys.stdin=open(input_file_path, "rt")
-n, m = map(int, input().split())
-li = [int(input()) for _ in range(n)]
-# 입력값
-# -----------------------------------------------------------------
 
-li.sort()
-lt, rt = 0, li[-1]
+def dfs(idx):
+    global visited, graph, answer
+    visited[idx]=True
+    answer+=1
 
-while lt<=rt:
-    mid=(lt+rt)//2
-    total=sum([l//mid for l in li])
-    if total>=m:
-        ans=mid
-        lt=mid+1
-    else:
-        rt=mid-1
+    for i in range(1, n+1):
+        if not visited[i] and graph[idx][i]:
+            dfs(i)
 
-print(ans)
+n = int(input())  # 노드의 개수
+m = int(input())  # 간선의 개수
+
+graph = [[False]*(n+1) for _ in range(n+1)]
+visited = [False]*(n+1)
+answer=0
+
+for _ in range(m):
+    i, j = map(int, input().split())
+    graph[i][j] = True
+    graph[j][i] = True
+
+dfs(1)
+
+print(answer-1)
