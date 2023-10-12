39fb7996216d07eba71f1044329971d53ff197a3
fkstndnjs <fkstndnjs@naver.com>
2023-10-12 14:04:07 +0900

    깊이 우선 탐색 1

--- lv2/ex17.py
+++ lv2/ex17.py
@@ -1,28 +1,33 @@
 import sys
 input_file_path = "/Users/ysh/Documents/Git Repository/CodingTest/lv2/input.txt"
 sys.stdin=open(input_file_path, "rt")
-n, e = map(int, input().split())
+
+N, M, R = map(int, input().split())
 
 def dfs(idx):
-    global graph, visited
-    visited[idx]=True
+    global visited, graph, answer, order
+    visited[idx] = True
+    answer[idx] = order
+    order += 1
 
-    for i in range(1, n+1):
-        if not visited[i] and graph[idx][i]:
+    for i in graph[idx]:  # 인접한 노드들을 순회합니다.
+        if not visited[i]:  # 방문하지 않은 노드인 경우에만 재귀 호출합니다.
             dfs(i)
 
-graph=[[False]*(n+1) for _ in range(n+1)]
-visited=[False]*(n+1)
-cnt=0
+graph = [[] for _ in range(N+1)]
+visited = [False]*(N+1)
+answer = [0]*(N+1)
+order=1
 
-for _ in range(e):
+for _ in range(M):
     x, y = map(int, input().split())
-    graph[x][y]=True
-    graph[y][x]=True
+    graph[x].append(y)
+    graph[y].append(x)
 
-for i in range(1, n+1):
-    if not visited[i]:
-        dfs(i)
-        cnt+=1
+for i in range(1, N+1):
+    graph[i] = sorted(graph[i])
 
-print(cnt)
+dfs(R)
+
+for i in range(1, N+1):
+    print(answer[i])
