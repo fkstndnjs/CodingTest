0bd2523e8a9ef99f56c5b5dfa361ae9a6435f1d1
fkstndnjs <fkstndnjs@naver.com>
2023-10-12 16:08:28 +0900

    1260: DFS BFS

--- lv2/ex17.py
+++ lv2/ex17.py
@@ -1,33 +1,45 @@
 import sys
+from collections import deque
 input_file_path = "/Users/ysh/Documents/Git Repository/CodingTest/lv2/input.txt"
+sys.setrecursionlimit(10**5)
 sys.stdin=open(input_file_path, "rt")
+input=sys.stdin.readline
 
-N, M, R = map(int, sys.stdin.readline().split())
+N, M, R = map(int, input().split())
 
 def dfs(idx):
     global count
     visited[idx]=True
-    answer[idx]=count
-    count+=1
+    print(idx, end=" ")
 
-    graph[idx].sort(reverse=True)
+    graph[idx].sort()
     for i in graph[idx]:
         if not visited[i]:
             dfs(i)
 
+def bfs(idx):
+    queue=deque([idx])
+    visited2[idx]=True
+
+    while queue:
+        v=queue.popleft()
+        print(v, end=" ")
+
+        for i in graph[v]:
+            if not visited2[i]:
+                queue.append(i)
+                visited2[i]=True
 
 graph=[[] for _ in range(N+1)]
 visited=[False]*(N+1)
-answer=[0]*(N+1)
-count=1
+visited2=[False]*(N+1)
 
 for _ in range(M):
-    x, y = map(int, sys.stdin.readline().split())
+    x, y = map(int, input().split())
     graph[x].append(y)
     graph[y].append(x)
 
 
 dfs(R)
-
-for i in range(1, N+1):
-    print(answer[i])
+print()
+bfs(R)
