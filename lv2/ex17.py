b28dd9563a93912451456b47a21df2c28d0ea43d
fkstndnjs <fkstndnjs@naver.com>
2023-10-12 17:21:37 +0900

    11725: 트리의 부모찾기

--- lv2/ex17.py
+++ lv2/ex17.py
@@ -5,30 +5,26 @@ sys.stdin=open(input_file_path, "rt")
 input=sys.stdin.readline
 
 N = int(input())
-X, Y = map(int, input().split())
-M = int(input())
 
-def dfs(idx, count):
+def dfs(idx):
     global answer
     visited[idx]=True
 
-    if idx==Y:
-        answer=count
-        return
-    
     for i in graph[idx]:
         if not visited[i]:
-            dfs(i, count+1)
+            answer[i]=idx
+            dfs(i)
 
 graph=[[] for _ in range(N+1)]
 visited=[False]*(N+1)
-answer=-1
+answer=[0]*(N+1)
 
-for _ in range(M):
+for _ in range(N-1):
     x, y = map(int, input().split())
     graph[x].append(y)
     graph[y].append(x)
 
-dfs(X, 0)
+dfs(1)
 
-print(answer)
+for i in range(2, N+1):
+    print(answer[i])
