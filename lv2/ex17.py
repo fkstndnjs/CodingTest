e16f38c2e3713ab300d9f5099fcb86908603e356
fkstndnjs <fkstndnjs@naver.com>
2023-10-12 15:03:47 +0900

    24480: 깊이 우선 탐색2

--- lv2/ex17.py
+++ lv2/ex17.py
@@ -2,30 +2,30 @@ import sys
 input_file_path = "/Users/ysh/Documents/Git Repository/CodingTest/lv2/input.txt"
 sys.stdin=open(input_file_path, "rt")
 
-N, M, R = map(int, input().split())
+N, M, R = map(int, sys.stdin.readline().split())
 
 def dfs(idx):
-    global visited, graph, answer, order
-    visited[idx] = True
-    answer[idx] = order
-    order += 1
+    global count
+    visited[idx]=True
+    answer[idx]=count
+    count+=1
 
-    for i in graph[idx]:  # 인접한 노드들을 순회합니다.
-        if not visited[i]:  # 방문하지 않은 노드인 경우에만 재귀 호출합니다.
+    graph[idx].sort(reverse=True)
+    for i in graph[idx]:
+        if not visited[i]:
             dfs(i)
 
-graph = [[] for _ in range(N+1)]
-visited = [False]*(N+1)
-answer = [0]*(N+1)
-order=1
+
+graph=[[] for _ in range(N+1)]
+visited=[False]*(N+1)
+answer=[0]*(N+1)
+count=1
 
 for _ in range(M):
-    x, y = map(int, input().split())
+    x, y = map(int, sys.stdin.readline().split())
     graph[x].append(y)
     graph[y].append(x)
 
-for i in range(1, N+1):
-    graph[i] = sorted(graph[i])
 
 dfs(R)
 
