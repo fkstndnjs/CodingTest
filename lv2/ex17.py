e566e47db7c0a826a3f35c1d8b415749e15c5704
fkstndnjs <fkstndnjs@naver.com>
2023-10-13 10:40:52 +0900

    4963: 섬의 개수

--- lv2/ex17.py
+++ lv2/ex17.py
@@ -3,40 +3,36 @@ input_file_path = "/Users/ysh/Documents/Git Repository/CodingTest/lv2/input.txt"
 sys.stdin=open(input_file_path, "rt")
 sys.setrecursionlimit(10**8)
 input=sys.stdin.readline
-
-dx=[-1, 1, 0,0]
-dy=[0,0,-1,1]
-MAX=1000+10
-M, N = map(int, input().split())
+dx = [-1, -1, -1, 0, 0, 1, 1, 1]
+dy = [-1, 0, 1, -1, 1, -1, 0, 1]
 
 def dfs(x, y):
-    global answer, M
-    visited[x][y]=True
+    map_[x][y]=False
 
-    if x==M:
-        answer="YES"
-        return
+    for i in range(8):
+        nx, ny = x+dx[i], y+dy[i]
 
-    for i in range(4):
-        newX = x+dx[i]
-        newY = y+dy[i]
+        if map_[nx][ny]:
+            dfs(nx, ny)
 
-        if map_[newX][newY] and not visited[newX][newY]:
-            dfs(newX, newY)
+while True:
+    cnt=0
+    m, n = map(int, input().split())
 
+    if m==0 and n==0:
+        break
 
-map_ = [[False]*MAX for _ in range(MAX)]
-visited = [[False]*MAX for _ in range(MAX)]
-answer="NO"
+    map_ = [[False]*(m+10) for _ in range(n+10)]
 
-for i in range(1, M+1):
-    p = input().strip()
-    for j in range(1, N+1):
-        map_[i][j] = p[j-1]=='0'
+    for i in range(1, n+1):
+        tmp = list(map(int, input().split()))
+        for j in range(1, m+1):
+            map_[i][j] = tmp[j-1]==1
 
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if map_[i][j]:
+                dfs(i,j)
+                cnt+=1
 
-for k in range(1, N+1):
-    if map_[1][k]:
-        dfs(1, k)
-
-print(answer)
+    print(cnt)
