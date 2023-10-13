a7e9416dcd496ff5f23d514f3d98d793c3c8e36b
fkstndnjs <fkstndnjs@naver.com>
2023-10-13 10:21:55 +0900

    fix

--- lv2/ex17.py
+++ lv2/ex17.py
@@ -1,30 +1,42 @@
 import sys
 input_file_path = "/Users/ysh/Documents/Git Repository/CodingTest/lv2/input.txt"
-sys.setrecursionlimit(10**5)
 sys.stdin=open(input_file_path, "rt")
+sys.setrecursionlimit(10**8)
 input=sys.stdin.readline
 
-N = int(input())
+dx=[-1, 1, 0,0]
+dy=[0,0,-1,1]
+MAX=1000+10
+M, N = map(int, input().split())
 
-def dfs(idx):
-    global answer
-    visited[idx]=True
+def dfs(x, y):
+    global answer, M
+    visited[x][y]=True
 
-    for i in graph[idx]:
-        if not visited[i]:
-            answer[i]=idx
-            dfs(i)
+    if x==M:
+        answer="YES"
+        return
 
-graph=[[] for _ in range(N+1)]
-visited=[False]*(N+1)
-answer=[0]*(N+1)
+    for i in range(4):
+        newX = x+dx[i]
+        newY = y+dy[i]
 
-for _ in range(N-1):
-    x, y = map(int, input().split())
-    graph[x].append(y)
-    graph[y].append(x)
+        if map_[newX][newY] and not visited[newX][newY]:
+            dfs(newX, newY)
 
-dfs(1)
 
-for i in range(2, N+1):
-    print(answer[i])
+map_ = [[False]*MAX for _ in range(MAX)]
+visited = [[False]*MAX for _ in range(MAX)]
+answer="NO"
+
+for i in range(1, M+1):
+    p = input().strip()
+    for j in range(1, N+1):
+        map_[i][j] = p[j-1]=='0'
+
+
+for k in range(1, N+1):
+    if map_[1][k]:
+        dfs(1, k)
+
+print(answer)
